3706cfe43a6e02bf259480e68d51e2e4e60fbaf8
Lilian Schall <lilian.schall@epita.fr>
2024-04-23 15:22:35 +0200

    feat: added tools for downloading easily dataset

---
+++ utils/download_dataset.py
@@ -0,0 +1,19 @@
+import os
+
+import s3fs
+import zipfile
+
+
+def download_dataset_if_needed(path: str = "./LivingRoom_preprocessed_hack"):
+    if os.path.exists(path):
+        print(f"path: {path} already exist, ignorign dataset downloading")
+
+    s3_endpoint_url = "https://" + os.environ["AWS_S3_ENDPOINT"]
+    fs = s3fs.S3FileSystem(client_kwars={"endpoint_url": s3_endpoint_url})
+
+    path_to_dataset = fs.ls("gvimont/diffusion/hackathon-minarm-2024")[0]
+
+    fs.download(path_to_dataset, ".")
+
+    with zipfile.ZipFile(f"./{os.path.basename(path_to_dataset)}", "r") as zip_file:
+        zip_file.extractall(path)
